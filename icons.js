02fc8ca009b916965ccf0bd0ec0d8ba92067733c
phil <uniphil@gmail.com>
2014-08-28 09:37:36 -0400

    drop the closure

--- icons.js
+++ icons.js
@@ -3,93 +3,89 @@
  */
 
 
-(function(L) {
+L._svgishDivIcon = L.Icon.extend({
 
-  var _svgishDivIcon = L.Icon.extend({
+  options: {
+    className: 'leaflet-div-style-icon',
+    html: '',
 
-    options: {
-      className: 'leaflet-div-style-icon',
-      html: '',
+    stroke: true,
+    color: '#3388ff',
+    weight: 3,
+    // opacity: 1,
+    lineCap: 'round',
+    lineJoin: 'round',
+    dashArray: null,
+    fill: true,
+    fillColor: null,  // same as color by default
+    // fillOpacity: 0.2,
+  },
 
-      stroke: true,
-      color: '#3388ff',
-      weight: 3,
-      // opacity: 1,
-      lineCap: 'round',
-      lineJoin: 'round',
-      dashArray: null,
-      fill: true,
-      fillColor: null,  // same as color by default
-      // fillOpacity: 0.2,
-    },
+  createIcon: function(oldIcon) {
+    var div = (oldIcon && oldIcon.tagName === 'DIV') ? oldIcon : document.createElement('div');
+    div.innerHTML = this.options.html;
+    this._setCssStyles(div, this.options);
+    this._setIconStyles(div, 'icon');
+    return div;
+  },
 
-    createIcon: function(oldIcon) {
-      var div = (oldIcon && oldIcon.tagName === 'DIV') ? oldIcon : document.createElement('div');
-      div.innerHTML = this.options.html;
-      this._setCssStyles(div, this.options);
-      this._setIconStyles(div, 'icon');
-      return div;
-    },
-
-    _setCssStyles: function(el, options) {
-      if (options.stroke) {
-        var borderTemplate = '{width}px {dasher} {color}';
-        var dasher = options.dashArray ? 'dashed' : 'solid';  // TODO: sometimes dotted?
-        el.style.border = L.Util.template(borderTemplate, {
-          width: options.weight,
-          dasher: dasher,
-          color: options.color
-          // TODO: opacity... use rgba color for now
-        });
-      }
-      if (options.fill) {
-        el.style.background = options.fillColor || options.color;
-        // TODO: fillOpacity... use rgba fillColor for now
-      }
-    },
-
-    createShadow: function() {
-      return null;
+  _setCssStyles: function(el, options) {
+    if (options.stroke) {
+      var borderTemplate = '{width}px {dasher} {color}';
+      var dasher = options.dashArray ? 'dashed' : 'solid';  // TODO: sometimes dotted?
+      el.style.border = L.Util.template(borderTemplate, {
+        width: options.weight,
+        dasher: dasher,
+        color: options.color
+        // TODO: opacity... use rgba color for now
+      });
     }
-
-  });
-
-
-  L.RectangleDivIcon = _svgishDivIcon.extend({
-    initialize: function(size, options) {
-      L.setOptions(this, options);
-      this._size = L.point(size);
-    },
-
-    _setCssStyles: function(el, options) {
-      el.style.width = this._size.x + 'px';
-      el.style.height = this._size.y + 'px';
-      return _svgishDivIcon.prototype._setCssStyles.apply(this, arguments);
+    if (options.fill) {
+      el.style.background = options.fillColor || options.color;
+      // TODO: fillOpacity... use rgba fillColor for now
     }
-  });
+  },
+
+  createShadow: function() {
+    return null;
+  }
+
+});
 
 
-  L.CircleDivIcon = _svgishDivIcon.extend({
-    initialize: function(radius, options) {
-      L.setOptions(this, options);
-      this._radius = radius;
-    },
+L.RectangleDivIcon = L._svgishDivIcon.extend({
+  initialize: function(size, options) {
+    L.setOptions(this, options);
+    this._size = L.point(size);
+  },
 
-    _setCssStyles: function(el, options) {
-      el.style.width = this._radius * 2 + 'px';
-      el.style.height = this._radius * 2 + 'px';
-      el.style.borderRadius = this._radius * 2 + 'px';
-      return _svgishDivIcon.prototype._setCssStyles.apply(this, arguments);
-    }
-  });
+  _setCssStyles: function(el, options) {
+    el.style.width = this._size.x + 'px';
+    el.style.height = this._size.y + 'px';
+    return L._svgishDivIcon.prototype._setCssStyles.apply(this, arguments);
+  }
+});
 
 
-  L.circleDivIcon = function(radius, options) {
-    return new L.CircleDivIcon(radius, options);
-  };
+L.CircleDivIcon = L._svgishDivIcon.extend({
+  initialize: function(radius, options) {
+    L.setOptions(this, options);
+    this._radius = radius;
+  },
 
-  L.rectangleDivIcon = function(size, options) {
-    return new L.RectangleDivIcon(size, options);
-  };
+  _setCssStyles: function(el, options) {
+    el.style.width = this._radius * 2 + 'px';
+    el.style.height = this._radius * 2 + 'px';
+    el.style.borderRadius = this._radius * 2 + 'px';
+    return L._svgishDivIcon.prototype._setCssStyles.apply(this, arguments);
+  }
+});
 
-})(window.L);
+
+L.circleDivIcon = function(radius, options) {
+  return new L.CircleDivIcon(radius, options);
+};
+
+L.rectangleDivIcon = function(size, options) {
+  return new L.RectangleDivIcon(size, options);
+};
